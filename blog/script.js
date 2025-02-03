6cf27e9549ac8247b0444d77f783a61a521c4983
Iko-Ojo <144394923+ikoojos@users.noreply.github.com>
2025-02-03 21:31:45 +1100

    Create script.js

---
+++ blog/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const darkModeToggle = document.getElementById("dark-mode-toggle");
+    darkModeToggle.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode");
+    });
+});
